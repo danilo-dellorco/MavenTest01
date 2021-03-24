dca7436dfed68c028f41797ad6a8c830922b657d
danilo-dellorco <danilodellorcolp@gmail.com>
2021-03-24 16:34:41 +0100

    commit progetto

---
+++ src/main/java/danilo/provamaven/App.java
@@ -0,0 +1,13 @@
+package danilo.provamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
